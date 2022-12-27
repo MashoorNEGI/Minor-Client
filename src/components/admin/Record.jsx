a1b2981f2696c61e1e22bb3eb74206c183faa784
MashoorNEGI <ayushdeepnegi@gmail.com>
2022-12-27 20:04:01 +0530

    admin->record page done

--- src/components/admin/Record.jsx
+++ src/components/admin/Record.jsx
@@ -1,17 +1,46 @@
 import axios from "axios";
+import { useState } from "react";
 import { useEffect } from "react";
+import { Button } from "@mui/material";
 
-const Record = async () => {
-    const res = await axios.get('users/Admin')
-    console.log(res.data);
+const Record = () => {
+    const [ data, setdata ] = useState([])
+    const read = async () => {
+        const res = await axios.get('users/Admin')
+        setdata(res.data)
+        // console.log(res.data[ 0 ]);
+    }
     useEffect(() => {
-        Record()
-    }, [0])
+        read()
+    }, [])
     return (
         <>
-            {
-                // res.data
-            }
+            <div className="tables">
+                <table className="table table-striped  table-hover">
+                    <thead>
+                        <tr>
+                            <th>Fac_ID</th>
+                            <th>Fac_Name</th>
+                            <th>Phone_no</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+
+                            data.map((data, index) =>
+                                <tr key={index}>
+                                    <td name={data.Fac_ID} value={data.Fac_ID}>{data.Fac_ID.slice(0, 3)}</td>
+                                    <td>{data.Fac_Name}</td>
+                                    <td>{data.Phone_no}</td>
+                                    <td>{data.Email}</td>
+                                </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
+
         </>
     );
 }
